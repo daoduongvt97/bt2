04ed47bb862e789611c5119e816fa8a8eb4a93a7
daoduongvt97 <daoduongvt971@gmail.com>
2020-10-01 16:46:52 +0700

    Update prefixcodetree.py

--- prefixcodetree.py
+++ prefixcodetree.py
@@ -3,9 +3,27 @@ class Node:
         self.l = None
         self.r = None
         self.v = val
-class PrefixCodeTree:
+class PrefixCodeTree(Node):
     def __init__(self):
         self.root = None
-    def __index__(self, root):
-        self.root = root
-    def insert(codeword, symbol):
+    def addNodeLeft(self,parent, val):
+        parent.l = Node(val)
+    def addNodeRight(self,parent, val):
+        parent.r = Node(val)
+    def insert(self,codeword, symbol):
+        if(self.root == None):
+            self.root = Node('')
+        nodeTemp = self.root
+        for ele in codeword:
+            if(ele == 0):
+                nodeTemp.l = Node(nodeTemp.l.v)
+                nodeTemp = nodeTemp.l
+            else:
+                nodeTemp.r = Node(nodeTemp.r.v)
+                nodeTemp = nodeTemp.r
+        nodeTemp.v = symbol
+    def decode(self, encodeedData, datalen):
+        data = encodeedData.split()
+        for i in range (0,datalen):
+
+Tree = PrefixCodeTree()
