9289cd0e48e4dee19c246e97433c32e9d10168e3
daoduongvt97 <daoduongvt971@gmail.com>
2020-10-02 09:09:43 +0700

    update

--- prefixcodetree.py
+++ prefixcodetree.py
@@ -3,27 +3,40 @@ class Node:
         self.l = None
         self.r = None
         self.v = val
+        self.isLeaf = True
 class PrefixCodeTree(Node):
     def __init__(self):
         self.root = None
-    def addNodeLeft(self,parent, val):
-        parent.l = Node(val)
-    def addNodeRight(self,parent, val):
-        parent.r = Node(val)
     def insert(self,codeword, symbol):
         if(self.root == None):
             self.root = Node('')
         nodeTemp = self.root
         for ele in codeword:
             if(ele == 0):
-                nodeTemp.l = Node(nodeTemp.l.v)
+                if(nodeTemp.l == None):
+                    nodeTemp.l = Node('')
+                nodeTemp.isLeaf = False
                 nodeTemp = nodeTemp.l
             else:
-                nodeTemp.r = Node(nodeTemp.r.v)
+                if(nodeTemp.r == None):
+                    nodeTemp.r = Node('')
+                nodeTemp.isLeaf = False
                 nodeTemp = nodeTemp.r
-        nodeTemp.v = symbol
+            nodeTemp.v = symbol
+    def getData(self,root, code):
+      return root.l if(code == '0') else root.r
     def decode(self, encodeedData, datalen):
-        data = encodeedData.split()
-        for i in range (0,datalen):
-
-Tree = PrefixCodeTree()
+        encodeedData = int.from_bytes(encodeedData, byteorder='big')
+        encodeedData = (bin(encodeedData)[2:])
+        result = []
+        nodeTemp = self.root
+        i = 0
+        while(i <= datalen):
+            if(nodeTemp.isLeaf == True):
+                result.append(nodeTemp.v)
+                nodeTemp = self.root
+                i = i-1
+            else:
+                nodeTemp = self.getData(nodeTemp, encodeedData[i])
+            i = i +1
+        return ''.join(result)
